8a21c2a794a05c4a322f3ff950e53cc5977a7412
mariahouse <housemaria04@gmail.com>
2023-01-06 15:58:34 -0500

    Annotations in a table now

--- iqengine-browse/src/Components/FileBrowser/File.jsx
+++ iqengine-browse/src/Components/FileBrowser/File.jsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import { formControlClasses } from '@mui/material';
 
 export default function FileRow({ info, updateConnectionMetaFileHandle, updateConnectionDataFileHandle, updateConnectionRecording }) {
   const [modal, setModal] = useState(false);
@@ -17,6 +18,26 @@ export default function FileRow({ info, updateConnectionMetaFileHandle, updateCo
     updateConnectionDataFileHandle(dataFileHandle);
     updateConnectionRecording(name);
   };
+
+  const displayData = info.annotations.map((item, index) => {
+    const deepItemCopy = JSON.parse(JSON.stringify(item));
+    delete deepItemCopy['core:sample_start'];
+    delete deepItemCopy['core:sample_count'];
+    delete deepItemCopy['core:freq_lower_edge'];
+    delete deepItemCopy['core:freq_upper_edge'];
+    delete deepItemCopy['core:description'];
+
+    return (
+      <tr key={index}>
+        <td>{item['core:sample_start']}</td>
+        <td>{item['core:sample_count']}</td>
+        <td>{item['core:freq_lower_edge']}</td>
+        <td>{item['core:freq_upper_edge']}</td>
+        <td>{item['core:description']}</td>
+        <td>{[JSON.stringify(deepItemCopy, null, 4)]}</td>
+      </tr>
+    );
+  });
 
   return (
     <tr>
@@ -38,20 +59,26 @@ export default function FileRow({ info, updateConnectionMetaFileHandle, updateCo
       <td className="align-middle">{info.sampleRate}</td>
       <td className="align-middle">
         <div>
-          <Button type="button" onClick={toggle}>
+          <Button type="button" variant="secondary" onClick={toggle}>
             {info.numberOfAnnotation}
           </Button>
 
           <Modal isOpen={modal} toggle={toggle} size="lg">
             <ModalHeader toggle={toggle}>{info.name}</ModalHeader>
             <ModalBody>
-              {info.annotations.map((item, index) => {
-                return (
-                  <div key={index}>
-                    <pre>{JSON.stringify(item, undefined, 4)}</pre>
-                  </div>
-                );
-              })}
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Sample Start</th>
+                    <th>Sample Count</th>
+                    <th>Frequency min (MHz)</th>
+                    <th>Frequency max (MHz)</th>
+                    <th>Description</th>
+                    <th>Other</th>
+                  </tr>
+                </thead>
+                <tbody>{displayData}</tbody>
+              </table>
             </ModalBody>
           </Modal>
         </div>
