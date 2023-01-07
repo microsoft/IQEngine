bf6c431af86405a547c7d9900ade3ea884e5556a
Marc L <marcll@vt.edu>
2023-01-06 19:28:30 -0500

    minor tweaks (#45)

--- iqengine-browse/src/Components/FileBrowser/File.jsx
+++ iqengine-browse/src/Components/FileBrowser/File.jsx
@@ -19,7 +19,7 @@ export default function FileRow({ info, updateConnectionMetaFileHandle, updateCo
     updateConnectionRecording(name);
   };
 
-  const displayData = info.annotations.map((item, index) => {
+  const annotationsData = info.annotations.map((item, index) => {
     const deepItemCopy = JSON.parse(JSON.stringify(item));
     delete deepItemCopy['core:sample_start'];
     delete deepItemCopy['core:sample_count'];
@@ -31,10 +31,10 @@ export default function FileRow({ info, updateConnectionMetaFileHandle, updateCo
       <tr key={index}>
         <td>{item['core:sample_start']}</td>
         <td>{item['core:sample_count']}</td>
-        <td>{item['core:freq_lower_edge']}</td>
-        <td>{item['core:freq_upper_edge']}</td>
+        <td>{item['core:freq_lower_edge'] / 1e6}</td>
+        <td>{item['core:freq_upper_edge'] / 1e6}</td>
         <td>{item['core:description']}</td>
-        <td>{[JSON.stringify(deepItemCopy, null, 4)]}</td>
+        <td>{JSON.stringify(deepItemCopy, null, 4).replaceAll('{', '').replaceAll('}', '').replaceAll('"', '')}</td>
       </tr>
     );
   });
@@ -71,13 +71,13 @@ export default function FileRow({ info, updateConnectionMetaFileHandle, updateCo
                   <tr>
                     <th>Sample Start</th>
                     <th>Sample Count</th>
-                    <th>Frequency min (MHz)</th>
-                    <th>Frequency max (MHz)</th>
+                    <th>Frequency Min [MHz]</th>
+                    <th>Frequency Max [MHz]</th>
                     <th>Description</th>
                     <th>Other</th>
                   </tr>
                 </thead>
-                <tbody>{displayData}</tbody>
+                <tbody>{annotationsData}</tbody>
               </table>
             </ModalBody>
           </Modal>
