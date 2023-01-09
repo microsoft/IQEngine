ed236dae652b76efea2b0e3cda36be1d56d5dd0a
Sreenaina Koujala <72166645+Nainicorn@users.noreply.github.com>
2023-01-09 15:32:41 -0500

    converted SettingsPane and App to functional (#78)

--- iqengine-browse/src/App.js
+++ iqengine-browse/src/App.js
@@ -8,24 +8,21 @@ import { Routes, Route, Link } from 'react-router-dom';
 import FileBrowserContainer from './Containers/FileBrowserContainer';
 import SpectrogramContainer from './Containers/SpectrogramContainer';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1 className="display-1">
-          <Link to="/">
-            <center>IQEngine</center>
-          </Link>
-        </h1>
+const App = () => {
+  return (
+    <div>
+      <h1 className="display-1">
+        <Link to="/">
+          <center>IQEngine</center>
+        </Link>
+      </h1>
 
-        <Routes>
-          <Route exact path="/" element={<FileBrowserContainer />} />
+      <Routes>
+        <Route exact path="/" element={<FileBrowserContainer />} />
 
-          <Route path="/spectrogram/:recording" element={<SpectrogramContainer />} />
-        </Routes>
-      </div>
-    );
-  }
-}
-
+        <Route path="/spectrogram/:recording" element={<SpectrogramContainer />} />
+      </Routes>
+    </div>
+  );
+};
 export default App;
