f38810db452df3923f42df1fe767b18c8831c837
Sreenaina Koujala <nkoujala@gmail.com>
2023-01-12 11:58:41 -0500

    we made the tiles info object into a map, and destructured props item

--- src/Components/FileBrowser/FileBrowser.js
+++ src/Components/FileBrowser/FileBrowser.js
@@ -11,30 +11,8 @@ import AzureBlobBrowser from './AzureBlobBrowser';
 import RepositoryTile from './RepositoryTile';
 
 const FileBrowser = (props) => {
-  const myobj = JSON.parse(process.env.REACT_APP_CONNECTION_INFO);
-
-
-  const name1 = myobj.settings[0]['name'];
-  const accountName1 = myobj.settings[0]['accountName'];
-  const containerName1 = myobj.settings[0]['containerName'];
-  const description1 = myobj.settings[0]['description'];
-  const imgUrl1 = myobj.settings[0]['imageURL'];
-  const sasToken1 = myobj.settings[0]['sasToken'];
-
-  const writeable1 = sasToken1.slice(sasToken1.search('sp')).split('&')[0].includes('w'); // boolean
-  const expires1 = sasToken1.slice(sasToken1.search('se')).split('&')[0].slice(3, 13); // YEAR-MONTH-DAY
-
-
-  const name2 = myobj.settings[1]['name'];
-  const accountName2 = myobj.settings[1]['accountName'];
-  const containerName2 = myobj.settings[1]['containerName'];
-  const description2 = myobj.settings[1]['description'];
-  const imgUrl2 = myobj.settings[1]['imageURL'];
-  const sasToken2 = myobj.settings[1]['sasToken'];
-
-
-  const writeable2 = sasToken2.slice(sasToken2.search('sp')).split('&')[0].includes('w'); // boolean
-  const expires2 = sasToken2.slice(sasToken2.search('se')).split('&')[0].slice(3, 13); // YEAR-MONTH-DAY
+  const tileObj = JSON.parse(process.env.REACT_APP_CONNECTION_INFO);
+  const tileObjInfo = tileObj.settings;
 
   return (
     <div>
@@ -62,28 +40,11 @@ const FileBrowser = (props) => {
           </Col>
         </Row>
         <Row>
-          <Col>
-            <RepositoryTile
-              name={name1}
-              accountName={accountName1}
-              containerName={containerName1}
-              description={description1}
-              imgUrl={imgUrl1}
-              writeable={writeable1}
-              expires={expires1}
-            ></RepositoryTile>
-          </Col>
-          <Col>
-            <RepositoryTile
-              name={name2}
-              accountName={accountName2}
-              containerName={containerName2}
-              description={description2}
-              imgUrl={imgUrl2}
-              writeable={writeable2}
-              expires={expires2}
-            ></RepositoryTile>
-          </Col>
+          {tileObjInfo.map((item, i) => (
+            <Col>
+              <RepositoryTile key={i} item={item}></RepositoryTile>
+            </Col>
+          ))}
         </Row>
       </Container>
 
--- src/Components/FileBrowser/RepositoryTile.js
+++ src/Components/FileBrowser/RepositoryTile.js
@@ -3,6 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Card, Col, Button } from 'react-bootstrap';
 
 const RepositoryTile = (props) => {
+  const { name, accountName, containerName, imageURL, description, sasToken } = props.item;
+  const expires = sasToken.slice(sasToken.search('se')).split('&')[0].slice(3, 13); // YEAR-MONTH-DAY
+  // const writeable1 = sasToken1.slice(sasToken1.search('sp')).split('&')[0].includes('w'); // boolean
+
   const styleHeight = {
     width: 300,
     height: 300,
@@ -20,17 +24,17 @@ const RepositoryTile = (props) => {
   return (
     <div className="App">
       <Container className="p-4">
-        <Col md="9">
+        <Col md="8">
           <Card>
             <Card.Body style={styleCenter}>
-              <Card.Img variant="top" src={props.imgUrl} style={styleHeight}></Card.Img>
+              <Card.Img variant="top" src={imageURL} style={styleHeight}></Card.Img>
               <center>
-                <Card.Title>{props.name}</Card.Title>
+                <Card.Title>{name}</Card.Title>
               </center>
-              <Card.Text style={styleObj}>Account Name: {props.accountName}</Card.Text>
-              <Card.Text style={styleObj}>Container Name: {props.containerName}</Card.Text>
-              <Card.Text style={styleObj}>{props.description}</Card.Text>
-              <Card.Text style={styleObj}>SAS Token expiration: {props.expires}</Card.Text>
+              <Card.Text style={styleObj}>Account Name: {accountName}</Card.Text>
+              <Card.Text style={styleObj}>Container Name: {containerName}</Card.Text>
+              <Card.Text style={styleObj}>{description}</Card.Text>
+              <Card.Text style={styleObj}>SAS Token expiration: {expires}</Card.Text>
             </Card.Body>
             <Button variant="success">Browse</Button>
           </Card>
@@ -39,7 +43,5 @@ const RepositoryTile = (props) => {
     </div>
   );
 };
-
-// JSON.stringify()
 
 export default RepositoryTile;
